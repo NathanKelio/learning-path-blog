c8970805872943222bcc2672167a0a4007aab969
NathanKelio <nathan.gadbin@kelio.com>
2025-05-14 14:59:35 +0200

    add published_at for draft, published, and scheduled blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_13_152525) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_14_114117) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
